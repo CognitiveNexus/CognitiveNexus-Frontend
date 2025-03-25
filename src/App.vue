36fe0f710c618be2e1e7cfb7eaa8b6d67e08ca8a
BobLiu <bobliu0518@gmail.com>
2025-03-26 00:02:43 +0800

    build: use auto tree-shaking

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+import { ElConfigProvider } from 'element-plus';
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
 import CodeRunner from '@/components/CodeRunner.vue';
 </script>
 
 <template>
-    <CodeRunner />
+    <ElConfigProvider :locale="zhCn">
+        <CodeRunner />
+    </ElConfigProvider>
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { createApp } from 'vue';
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
-import zhCn from 'element-plus/es/locale/lang/zh-cn';
 import App from './App.vue';
+import 'element-plus/dist/index.css';
 
 const app = createApp(App);
-
-app.use(ElementPlus, { locale: zhCn }).mount('#app');
+app.mount('#app');
