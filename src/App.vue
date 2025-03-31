9c7e64b2e02446b0e924941ad70de2a5ed556a63
BobLiu <bobliu@bobliu.tech>
2025-03-31 19:41:59 +0800

    feat: set element plus language as zh-cn

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
+  <el-config-provider :locale="zhCn">
     <el-container class="main-container">
-        <AppHeader />
-        <el-container>
-            <RouterView></RouterView>
-        </el-container>
-        <el-footer></el-footer>
-    </el-container>
+      <AppHeader />
+      <el-container>
+        <RouterView></RouterView>
+      </el-container>
+      <el-footer></el-footer> </el-container
+  ></el-config-provider>
 </template>
 
 <script setup lang="ts" name="App">
 import { RouterView } from 'vue-router';
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
 </script>
 
 <style scoped>
 .main-container {
-    font-family: Inter, 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
-    height: 100vh;
+  font-family: Inter, 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
+  height: 100vh;
 }
 .el-menu-vertical:not(.el-menu--collapse) {
-    height: 100%;
+  height: 100%;
 }
 .el-footer {
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.151);
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.151);
 }
 </style>
