d968806147b3381774d206921d4f0a00ca37a1d0
BobLiu <bobliu0518@gmail.com>
2025-04-11 00:09:44 +0800

    feat: optimize animation

--- src/components/animation/EnterAnimation.vue
+++ src/components/animation/EnterAnimation.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="main">
     <motion.div
+      :key="animationKey"
       :initial="{ opacity: 0, scale: 0 }"
       :animate="{ opacity: 1, scale: 1 }"
       :whileHover="{ scale: 1.2 }"
@@ -9,8 +10,7 @@
         duration: 0.4,
         scale: { type: 'spring', visualDuration: 0.4, bounce: 0.5 },
       }"
-      class="ani"
-    >
+      class="ani">
       <img :src="src" :alt="alt" />
     </motion.div>
   </div>
@@ -18,22 +18,38 @@
 
 <script setup lang="ts" name="EnterAnimation">
 import { motion } from 'motion-v';
+import { ref, watch } from 'vue';
 
 const props = defineProps({
   src: String,
   alt: String,
 });
+
+const animationKey = ref(0);
+
+watch(
+  () => props.src,
+  () => {
+    animationKey.value++;
+  }
+);
 </script>
 
 <style scoped>
 .main {
   width: 100%;
   height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 .ani {
   width: 100%;
   height: 100%;
   overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 img {
   width: 500px;
